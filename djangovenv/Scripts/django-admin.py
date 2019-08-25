a2a0c6be03455dfecb0bc205145add9fb91fbe4f
ankitjaadoo <talk2ankit007@gmail.com>
2019-08-25 18:33:13 +0530

    My Django app, First Commit

---
+++ djangovenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hp\desktop\100daysofcode\learningbydoing\django_app\djangovenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
